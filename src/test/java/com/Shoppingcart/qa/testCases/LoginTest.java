fd35ee281a666be77ebb9dcfeeca21f86a41fd73
Nandha Kumar <nandhanaveen845@gmail.com>
2023-03-26 12:37:09 +0530

    New Email Demo poject

---
+++ src/test/java/com/Shoppingcart/qa/testCases/LoginTest.java
@@ -0,0 +1,10 @@
+package com.Shoppingcart.qa.testCases;
+
+public class LoginTest {
+
+  // sample code for login test
+  // added new code
+  // new update
+	
+	// this is a replica of master branch xyz
+}
